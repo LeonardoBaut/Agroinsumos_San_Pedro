fb376b3aa243f73bc35b18787bb0eb6840908b16
LeonardoBaut <bacl050415@gs.utm.mx>
2025-11-23 17:12:23 -0600

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -32,13 +32,12 @@ db.categorias.insertOne({ nombre_categoria: "FERTILIZANTES", direccion_img: "img
 db.categorias.insertOne({ nombre_categoria: "HERBICIDAS", direccion_img: "imgs/herbicidas.jpg" });
 db.categorias.insertOne({ nombre_categoria: "ADHERENTES",  direccion_img: "imgs/adherentes.jpg" });
 
+db.marcas.insertOne({ id_marcas: "1", nombre_marca: "BAYER", direccion_img: "imgs/bayer.jpg" });
+db.marcas.insertOne({ id_marcas: "2", nombre_marca: "SYNGENTA", direccion_img: "imgs/sygenta.jpg" });
+db.marcas.insertOne({ id_marcas: "3", nombre_marca: "ULTRASOL", direccion_img: "imgs/ultrasol.jpg" });
+db.marcas.insertOne({ id_marcas: "4", nombre_marca: "AGROENZYMAS", direccion_img: "imgs/agroenzymas.jpg" });
 
-db.marcas.insertOne({ nombre: "BAYER", img: "imgs/bayer.jpg" });
-db.marcas.insertOne({ nombre: "SYNGENTA", img: "imgs/sygenta.jpg" });
-db.marcas.insertOne({ nombre: "ULTRASOL", img: "imgs/ultrasol.jpg" });
-db.marcas.insertOne({ nombre: "AGROENZYMAS", img: "imgs/agroenzymas.jpg" });
-
-db.ingredientes.insertOne({ nombre: "GLIFOSATO", img: "imgs/ingrediente.jpg" });
-db.ingredientes.insertOne({ nombre: "ATRAZINA", img: "imgs/ingrediente.jpg" });
-db.ingredientes.insertOne({ nombre: "IMIDACLOPRID", img: "imgs/ingrediente.jpg" });
-db.ingredientes.insertOne({ nombre: "PARAQUAT", img: "imgs/ingrediente.jpg" });
+db.ingredientes.insertOne({ id_ingrediente: "1", nombre_ingrediente: "GLIFOSATO", direccion_img: "imgs/ingrediente.jpg" });
+db.ingredientes.insertOne({ id_ingrediente: "2", nombre_ingrediente: "ATRAZINA", direccion_img: "imgs/ingrediente.jpg" });
+db.ingredientes.insertOne({ id_ingrediente: "3", nombre_ingrediente: "IMIDACLOPRID", direccion_img: "imgs/ingrediente.jpg" });
+db.ingredientes.insertOne({ id_ingrediente: "4", nombre_ingrediente: "PARAQUAT", direccion_img: "imgs/ingrediente.jpg" });
